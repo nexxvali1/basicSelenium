2bb61a9136dd3ca3b5889ba3f0001a1455b38657
Mukit <Mukit@syeds-air.attlocal.net>
2022-04-12 12:41:05 -0500

    Read from Prop file with TestNG annotations

---
+++ src/test/java/selenium/test/nexxvali/FunWithTestNgAnnoPropFile.java
@@ -0,0 +1,54 @@
+package selenium.test.nexxvali;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class FunWithTestNgAnnoPropFile {
+
+	String chromeDriver = "webdriver.chrome.driver";
+	String driverLocation = "/Users/Mukit/Documents/Selenium/chromedriver";
+	static String propertyFileLocation = "target/Locators.properties";
+	public WebDriver driver;
+	public Properties prop;
+
+	@BeforeTest
+	public void launchBrowser() throws Exception {
+		InputStream input = new FileInputStream(propertyFileLocation);
+		prop = new Properties();
+		prop.load(input);
+
+		System.setProperty("webdriver.chrome.driver", driverLocation);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get(prop.getProperty("nexxvaliURL"));
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void clickOnAboutUs() {
+		driver.findElement(By.xpath(prop.getProperty("aboutUsBtnXpath"))).click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		String actAbtUsPgTitle = driver.getTitle();
+		String expAbtUsPgTitle = "About Us – Nexxvali";
+		Assert.assertEquals(actAbtUsPgTitle, expAbtUsPgTitle);
+
+	}
+
+	@AfterTest
+	public void terminateBrowser() {
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.close();
+	}
+}
