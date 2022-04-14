9057c163e416109dbba78e0b6ef70afaceb9b766
Mukit <Mukit@syeds-air.attlocal.net>
2022-04-14 11:22:47 -0500

    Additional @Test params

--- src/test/java/selenium/test/nexxvali/FunWithTestNgAnnoPropFile.java
+++ src/test/java/selenium/test/nexxvali/FunWithTestNgAnnoPropFile.java
@@ -1,6 +1,5 @@
 package selenium.test.nexxvali;
 
-import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
